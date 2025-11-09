72e7aa310c09ff85eb06164bfbe73f076ce8c14f
Mateo Chavez Pinto <99995064+a01639510@users.noreply.github.com>
2025-11-09 00:33:18 -0600

    Update 3_Analisis.py

--- pages/3_Analisis.py
+++ pages/3_Analisis.py
@@ -8,3 +8,10 @@ if st.session_state.df_ventas is None:
     st.warning("Sube datos en **Archivos**")
 else:
     analytics_app()
+with st.sidebar:
+    st.image("https://via.placeholder.com/150", caption="Stock Zero")
+    st.page_link("stock_zero_mvp.py", label="Home")
+    st.page_link("pages/1_Archivos.py", label="Archivos")
+    st.page_link("pages/2_Inventario.py", label="Inventario")
+    st.page_link("pages/3_Analisis.py", label="Análisis")
+    st.page_link("pages/4_Productos.py", label="Productos")
